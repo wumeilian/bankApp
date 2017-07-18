3eeba8b97a34a54ce292598beae4c0672a2c7190
wumeilian <1054527951@qq.com>
2017-07-19 00:36:01 +0800

    新增贵金属详情页页面及数据

--- asset/js/pmc.js
+++ asset/js/pmc.js
@@ -1,6 +1,11 @@
 /*贵金属页*/
 $(document).ready(function(){
-    initList()  //贵金属列表
+  if($('#pmcHome').length > 0) {
+      initList()  //贵金属列表
+  }
+  if($('#pmcDetail').length > 0) {
+    initDetail() //详情页
+  }
 })
 
 //贵金属列表数据
@@ -8,6 +13,7 @@ function initList() {
   if(data.Code == 0 && data.Items.length > 0) {
       var html = data.Items.map(function(items) {
           return Mustache.render(template.listTemplate,{
+          id: items.id,
           imgurl: items.imgurl,
           title: items.title,
           middle: '零售价：￥' + items.price,
@@ -15,6 +21,51 @@ function initList() {
       })
       $('#pmcHome .mod-list').html(html)
   }
+}
+//积分详情页
+function initDetail() {
+  // $.getJSON('url', {
+  //     id: value,
+  //     t: (new Date()).getTime()
+  // }, function(data) {
+
+    if(dataDetail.Code == 0 && dataDetail.Items) {
+      var data = dataDetail.Items
+      var slide_html = data.slideImg.map(function(items) {
+            return Mustache.render(tem_slide,{
+            imgurl: items})
+        })
+
+        $('#pmcDetail .swiper-wrapper').html(slide_html)
+        initSlide() //轮播图
+
+        var info_html = Mustache.render(tem_info,{
+          name: data.name,
+          price: data.price,
+          add: data.add
+        })
+        $('#pmcDetail .pmcdetail-des').html(info_html)
+
+        // var img_html = data.detailImg.map(function(items) {
+        //       return Mustache.render(tem_img,{
+        //       imgurl: items})
+        //   })
+        // $('#creditDetail .img').html(img_html)
+        // var order_url = 'order.html?n=' + encodeURI(data.name) +
+        //   '&c=' + encodeURI(data.credit) +'&p=' + encodeURI(data.price) +
+        //     '&u=' + encodeURI(data.imgurl)
+        // $('#creditDetail .change').attr('href', order_url)
+    }
+// })
+}
+/*轮播图*/
+function initSlide() {
+  var mySwiper = new Swiper ('.swiper-container', {
+    loop: true,
+    // 如果需要分页器
+    pagination: '.swiper-pagination',
+
+  })
 }
 
 var data = {
@@ -49,4 +100,20 @@ var data = {
         'imgurl': '../../asset/images/pmc2.png'
     },
   ]
+}
+
+//贵金属详情页数据
+var dataDetail = {
+  Code: 0,
+  Items: {
+    'id': 1,
+    'name': '纯金世纪龙钞',
+    'price': 1650.00,
+    'add': '湖南长沙',
+    'slideImg': [
+      '../../asset/images/pmc_d1.png',
+      '../../asset/images/pmc_d1.png',
+      '../../asset/images/pmc_d1.png'
+    ]
+  }
 }
