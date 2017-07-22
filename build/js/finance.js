8c493377f02f9e8530cd5c3ce8e32067e8dd6bea
wumeilian <1054527951@qq.com>
2017-07-23 00:15:36 +0800

    基金详情页数据处理

--- build/js/finance.js
+++ build/js/finance.js
@@ -29,42 +29,27 @@ function initDetail() {
         //折线图
         line(data.date, data.num)
         //产品详情
-        var html_detail = data.detail.map(function(items,index) {
-            return Mustache.render(temp_detail,{
-              index: index,
-              items: items})
+        var html_deshead = data.headDes.map(function(items) {
+            return Mustache.render(temp_des,{
+              key: items.key,
+              value: items.value})
         })
-        // var html_detail =  Mustache.render(temp_detail,{
-        //   name: data.name,
-        //   code: data.code,
-        //   manage: data.manage,
-        //   theme: data.theme,
-        //   proName: data.proName,
-        //   proCode: data.proCode,
-        //   proCurrency: data.proCurrency,
-        //   proIncome: data.proIncome,
-        //   proValue: data.proValue,
-        //   proTime: data.proTime,
-        //   proObj: data.proObj,
-        //   proStatus: data.proStatus,
-        //   proType: data.proType,
-        //   proIncomeType: data.proIncomeType,
-        //   proLevel: data.proLevel,
-        //   proDate: data.proDate,
-        //   proBegin: data.proBegin,
-        //   proEnd: data.proEnd,
-        //   proChannel: data.proChannel,
-        //   proExchange: data.proExchange,
-        //   proGuadan: data.proGuadan,
-        //   payOrigin: data.payOrigin,
-        //   payAdd: data.payAdd,
-        //   payBack: data.payBack,
-        //   payLow: data.payLow,
-        //   payRule: data.payRule
-        // })
-        $('#financeDetail .detailContent').html(html_detail)
+        $('#financeDetail .detail-deshead').html(html_deshead)
+
+        var html_despro = data.modPro.map(function(items) {
+            return Mustache.render(tem_mod,{
+              key: items.key,
+              value: items.value})
+        })
+        $('#financeDetail .pro .info-txt').html(html_despro)
+
+        var html_despay = data.modPay.map(function(items) {
+            return Mustache.render(tem_mod,{
+              key: items.key,
+              value: items.value})
+        })
+        $('#financeDetail .pay .info-txt').html(html_despay)
     }
-  // })
 }
 //折线图
 function line(arrayX, arrayY) {
@@ -140,35 +125,87 @@ var dataDetail = {
         'title': '农商宝',
         'date': ['06-11', '06-11', '06-11', '06-11', '06-11', '06-11', '06-11'],
         'num': [3,5,2,5,2,4,6],
-        'detai': [{
-          'name': '博时现金宝货币C类',
-          'code': '002855',
-          'manage': '博时基金管理有限公司',
-          'theme': '博时基金管理有限公司',
-          'proName': '掌中宝-001期',
-          'proCode': 'LEB001',
-          'proCurrency': '人民币',
-          'proIncome': '4%',
-          'proValue': 1.0000,
-          'proTime': '36天',
-          'proObj': '适用于风险等级为稳健型（含）以上的客户',
-          'proStatus': '在售产品',
-          'proType': '现金管理类产品',
-          'proIncomeType': '保本浮动收益',
-          'proLevel': '低风险',
-          'proDate': '2017/01/27-2017/01/31',
-          'proBegin': '2017/02/01',
-          'proEnd': '2017/03/08',
-          'proChannel': '柜台，网上银行，手机银行',
-          'proExchange': '08:00:00-18:00:00',
-          'proGuadan': '18:00:00-07:59:59',
-          'payOrigin': '5万',
-          'payAdd': '1000.00',
-          'payBack': '1000.00',
-          'payLow': '5000.00',
-          'payRule': 'T日认购，资金冻结，投资收益起算日统一扣划；申购实施扣款到本金及收益到账规则'
+        'headDes': [{
+          'key': '基金名称',
+          'value': '博时现金宝货币C类'
+          },{
+            'key': '基金代码',
+            'value': '002855'
+        },{
+          'key': '基金管理人',
+          'value': '博时基金管理有限公司'
+        },{
+          'key': '基金销售服务主题',
+          'value': '博时基金管理有限公司'
+        }],
+        'modPro': [{
+          'key': '产品名称',
+          'value': '掌中宝-001期'
+        },{
+          'key': '产品代码',
+          'value': 'LEB001'
+        },{
+          'key': '产品币种',
+          'value': '人民币'
+        },{
+          'key': '预期年化收益',
+          'value': '4%'
+        },{
+          'key': '份额面值',
+          'value': 1.0000
+        },{
+          'key': '产品期限',
+          'value': '36天'
+        },{
+          'key': '使用对象',
+          'value': '适用于风险等级为稳健型（含）以上的客户'
+        },{
+          'key': '销售状态',
+          'value': '在售产品'
+        },{
+          'key': '产品类型',
+          'value': '现金管理类产品'
+        },{
+          'key': '收益类型',
+          'value': '保本浮动收益'
+        },{
+          'key': '风险等级',
+          'value': '低风险'
+        },{
+          'key': '产品销售期',
+          'value': '2017/01/27-2017/01/31'
+        },{
+          'key': '产品成立日',
+          'value': '2017/02/01'
+        },{
+          'key': '产品到期日',
+          'value': '2017/03/08'
+        },{
+          'key': '交易渠道',
+          'value': '柜台，网上银行，手机银行'
+        },{
+          'key': '交易时间',
+          'value': '08:00:00-18:00:00'
+        },{
+          'key': '挂单时间',
+          'value': '18:00:00-07:59:59'
+        }],
+        'modPay': [{
+          'key': '首次购买起点金额（起购金额）',
+          'value': '5万'
+        },{
+          'key': '追加购买起点金额',
+          'value': '1000.00'
+        },{
+          'key': '赎回起点份额',
+          'value': '1000.00'
+        },{
+          'key': '最低持有份额',
+          'value': '5000.00'
+        },{
+          'key': '购买扣款规则',
+          'value': 'T日认购，资金冻结，投资收益起算日统一扣划；申购实施扣款到本金及收益到账规则'
         }]
-
     }
 
 }
